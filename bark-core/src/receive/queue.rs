0288336f972f107c1f54cd5fbc8e343467698055
Hailey Somerville <hailey@hailey.lol>
2023-10-17 16:28:44 +1100

    move delaystart logic into bark-core queue, impl queue reset logic

--- bark-core/src/receive/queue.rs
+++ bark-core/src/receive/queue.rs
@@ -1,30 +1,41 @@
-use bark_protocol::packet::Audio;
+use core::num::NonZeroU16;
+
+use bark_protocol::{packet::Audio, types::AudioPacketHeader, time::{SampleDuration, Timestamp}};
 use heapless::Deque;
 
 use crate::consts::MAX_QUEUED_DECODE_SEGMENTS;
-
-pub enum NoSlot {
-    InPast,
-    TooFarInFuture,
-}
 
 pub struct PacketQueue {
     queue: Deque<Option<Audio>, MAX_QUEUED_DECODE_SEGMENTS>,
     /// The seq of the first packet in the queue, the rest are implied
     head_seq: u64,
+    /// We delay yielding packets when a queue is first started (or reset), to
+    /// allow for some buffering. The amount of packets buffered depends on
+    /// the difference between dts and pts in the initial packet.
+    start: DelayStart,
+}
+
+enum NoSlot {
+    InPast,
+    TooFarInFuture,
 }
 
 impl PacketQueue {
-    pub fn new(start_seq: u64) -> Self {
+    pub fn new(initial: &AudioPacketHeader) -> Self {
         PacketQueue {
             queue: Deque::new(),
-            head_seq: start_seq,
+            head_seq: initial.seq,
+            start: DelayStart::init(initial),
         }
     }
 
     pub fn pop_front(&mut self) -> Option<Audio> {
-        self.head_seq += 1;
-        self.queue.pop_front().flatten()
+        if self.start.yield_packet() {
+            self.head_seq += 1;
+            self.queue.pop_front().flatten()
+        } else {
+            None
+        }
     }
 
     pub fn insert_packet(&mut self, packet: Audio) {
@@ -43,7 +54,14 @@ impl PacketQueue {
                 log::warn!("received packet in past, dropping: head_seq={head_seq}, packet_seq={packet_seq}");
             }
             Err(NoSlot::TooFarInFuture) => {
-                log::warn!("received packet too far in future, dropping: tail_seq={tail_seq}, packet_seq={packet_seq}");
+                log::warn!("received packet too far in future, resetting queue: tail_seq={tail_seq}, packet_seq={packet_seq}");
+
+                // reset queue:
+                self.head_seq = packet_seq;
+                self.start = DelayStart::init(packet.header());
+                self.queue.clear();
+                self.queue.push_back(Some(packet)).expect("always room in queue after clear");
+
             }
         }
     }
@@ -69,5 +87,44 @@ impl PacketQueue {
         } else {
             Ok(&mut slices.1[idx - slices.0.len()])
         }
+    }
+}
+
+enum DelayStart {
+    Delay(NonZeroU16),
+    Live,
+}
+
+impl DelayStart {
+    pub fn init(header: &AudioPacketHeader) -> Self {
+        // calculate the stream delay by taking the difference between
+        // pts and dts in the initial packet:
+        let initial_pts = Timestamp::from_micros_lossy(header.pts);
+        let initial_dts = Timestamp::from_micros_lossy(header.dts);
+        let delay = initial_pts.saturating_duration_since(initial_dts);
+
+        // calculate number of packets this delay represents:
+        let packet_delay = delay.to_frame_count() / SampleDuration::ONE_PACKET.to_frame_count();
+
+        // quick n dirty round up:
+        let packet_delay = packet_delay + 1;
+
+        // calculate how many packets we should wait for before starting to
+        // yield audio segments to the decoder. this allows some time to build
+        // a buffer before beginning:
+        u16::try_from(packet_delay)
+            .and_then(NonZeroU16::try_from)
+            .map(DelayStart::Delay)
+            .unwrap_or(DelayStart::Live)
+    }
+
+    pub fn yield_packet(&mut self) -> bool {
+        if let DelayStart::Delay(count) = self {
+            *self = NonZeroU16::new(count.get() - 1)
+                .map(DelayStart::Delay)
+                .unwrap_or(DelayStart::Live);
+        }
+
+        matches!(self, DelayStart::Live)
     }
 }
--- bark/src/receive/stream.rs
+++ bark/src/receive/stream.rs
@@ -1,8 +1,6 @@
-use std::num::NonZeroU16;
-
 use bark_core::decode::AudioSegment;
 use bark_core::receive::timing::Timing;
-use bark_protocol::time::{TimestampDelta, Timestamp, SampleDuration};
+use bark_protocol::time::{TimestampDelta, Timestamp};
 use bark_protocol::types::{SessionId, AudioPacketHeader};
 use bark_protocol::packet::{Time, Audio};
 
@@ -12,42 +10,17 @@ pub struct Stream {
     sid: SessionId,
     timing: Timing,
     queue: PacketQueue,
-    start: DelayStart,
-}
-
-enum DelayStart {
-    Delay(NonZeroU16),
-    Live,
 }
 
 impl Stream {
     pub fn new(header: &AudioPacketHeader) -> Self {
-        // calculate the stream delay by taking the difference between
-        // pts and dts in the initial packet:
-        let initial_pts = Timestamp::from_micros_lossy(header.pts);
-        let initial_dts = Timestamp::from_micros_lossy(header.dts);
-        let delay = initial_pts.saturating_duration_since(initial_dts);
-
-        // calculate number of packets this delay represents:
-        let packet_delay = delay.to_frame_count() / SampleDuration::ONE_PACKET.to_frame_count();
-        // quick n dirty round up:
-        let packet_delay = packet_delay + 1;
-        // calculate how many packets we should wait for before starting to
-        // yield audio segments to the decoder. this allows some time to build
-        // a buffer before beginning:
-        let start = u16::try_from(packet_delay)
-            .and_then(NonZeroU16::try_from)
-            .map(DelayStart::Delay)
-            .unwrap_or(DelayStart::Live);
-
         // create packet queue data structure:
-        let queue = PacketQueue::new(header.seq);
+        let queue = PacketQueue::new(header);
 
         Stream {
             sid: header.sid,
             timing: Timing::default(),
             queue,
-            start,
         }
     }
 
@@ -64,19 +37,6 @@ impl Stream {
     }
 
     pub fn next_audio_segment(&mut self) -> Option<AudioSegment> {
-        // check delay start,
-        // decrement 1 from remaining packet count if in delay
-        match self.start {
-            DelayStart::Live => {}
-            DelayStart::Delay(count) => {
-                self.start = NonZeroU16::new(count.get() - 1)
-                    .map(DelayStart::Delay)
-                    .unwrap_or(DelayStart::Live);
-
-                return None;
-            }
-        }
-
         let packet = self.queue.pop_front()?;
 
         // if we haven't received any timing information yet, play it
