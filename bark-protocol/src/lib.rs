7e0748bb03b147508b04e97a6d2ed1b1571390be
Hailey Somerville <hailey@hailey.lol>
2023-10-25 15:45:19 +1100

    set FRAMES_PER_PACKET to 120

--- bark-protocol/src/lib.rs
+++ bark-protocol/src/lib.rs
@@ -7,7 +7,7 @@ pub mod types;
 
 pub const SAMPLE_RATE: SampleRate = SampleRate(48000);
 pub const CHANNELS: ChannelCount = ChannelCount(2);
-pub const FRAMES_PER_PACKET: usize = 160;
+pub const FRAMES_PER_PACKET: usize = 120; // 2.5ms at 48khz, compatible with opus
 pub const SAMPLES_PER_PACKET: usize = CHANNELS.0 as usize * FRAMES_PER_PACKET;
 
 #[derive(Copy, Clone, Debug)]
