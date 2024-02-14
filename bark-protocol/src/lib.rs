80c7dce809a5759ae283ad424248d1c9c4ad4ad2
Hailey Somerville <hailey@hailey.lol>
2024-02-14 23:35:33 +1100

    use derive_more::Into for all these

--- bark-protocol/src/lib.rs
+++ bark-protocol/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use derive_more::Into;
+
 pub mod buffer;
 pub mod packet;
 pub mod time;
@@ -10,56 +12,16 @@ pub const CHANNELS: ChannelCount = ChannelCount(2);
 pub const FRAMES_PER_PACKET: usize = 120; // 2.5ms at 48khz, compatible with opus
 pub const SAMPLES_PER_PACKET: usize = CHANNELS.0 as usize * FRAMES_PER_PACKET;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Into)]
+#[into(types(u64, u128, i64, f64))]
 pub struct SampleRate(pub u32);
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Into)]
+#[into(types(usize, u32, u64))]
 pub struct ChannelCount(pub u16);
 
 impl From<SampleRate> for usize {
     fn from(value: SampleRate) -> Self {
         value.0.try_into().expect("SampleRate -> usize")
-    }
-}
-
-impl From<SampleRate> for u32 {
-    fn from(value: SampleRate) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<SampleRate> for u64 {
-    fn from(value: SampleRate) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<SampleRate> for u128 {
-    fn from(value: SampleRate) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<SampleRate> for i64 {
-    fn from(value: SampleRate) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<ChannelCount> for usize {
-    fn from(value: ChannelCount) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<ChannelCount> for u64 {
-    fn from(value: ChannelCount) -> Self {
-        value.0.into()
-    }
-}
-
-impl From<ChannelCount> for u32 {
-    fn from(value: ChannelCount) -> Self {
-        value.0.into()
     }
 }
