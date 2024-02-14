655e00941c5af6b31bd0ade7e6e0fc7dc9425e91
Hailey Somerville <hailey@hailey.lol>
2024-02-14 16:28:47 +1100

    implement monotonic time for macos

--- bark/src/time.rs
+++ bark/src/time.rs
@@ -1,14 +1,23 @@
-use nix::sys::time::TimeValLike;
+use nix::sys::time::{TimeSpec, TimeValLike};
 use nix::time::ClockId;
 
 use bark_protocol::types::TimestampMicros;
 
 pub fn now() -> TimestampMicros {
-    let timespec = nix::time::clock_gettime(ClockId::CLOCK_MONOTONIC_RAW)
-        .expect("clock_gettime(CLOCK_MONOTONIC_RAW) failed, are we on Linux?");
+    let timespec = monotonic_time();
 
     let micros = u64::try_from(timespec.num_microseconds())
         .expect("cannot convert i64 time value to u64");
 
     TimestampMicros(micros)
+}
+
+#[cfg(target_os = "linux")]
+fn monotonic_time() -> TimeSpec {
+    nix::time::clock_gettime(ClockId::CLOCK_MONOTONIC_RAW).unwrap()
+}
+
+#[cfg(target_os = "macos")]
+fn monotonic_time() -> TimeSpec {
+    nix::time::clock_gettime(ClockId::from(libc::CLOCK_UPTIME_RAW)).unwrap()
 }
