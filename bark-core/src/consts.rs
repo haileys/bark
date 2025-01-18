379196ad477e51e77977647f4053daf977e0e191
Hailey Somerville <hailey@hails.org>
2025-01-18 14:44:11 +1100

    set FRAMES_PER_PACKET = 48

--- bark-core/src/consts.rs
+++ bark-core/src/consts.rs
@@ -1,4 +1,4 @@
 use bark_protocol::FRAMES_PER_PACKET;
 
-pub const MAX_QUEUED_DECODE_SEGMENTS: usize = 48;
+pub const MAX_QUEUED_DECODE_SEGMENTS: usize = 1024;
 pub const DECODE_BUFFER_FRAMES: usize = FRAMES_PER_PACKET * 2;
--- bark-protocol/src/lib.rs
+++ bark-protocol/src/lib.rs
@@ -9,7 +9,8 @@ pub mod types;
 
 pub const SAMPLE_RATE: SampleRate = SampleRate(48000);
 pub const CHANNELS: ChannelCount = ChannelCount(2);
-pub const FRAMES_PER_PACKET: usize = 120; // 2.5ms at 48khz, compatible with opus
+// pub const FRAMES_PER_PACKET: usize = 120; // 2.5ms at 48khz, compatible with opus
+pub const FRAMES_PER_PACKET: usize = 48;
 pub const SAMPLES_PER_PACKET: usize = CHANNELS.0 as usize * FRAMES_PER_PACKET;
 
 #[derive(Copy, Clone, Debug, Into)]
